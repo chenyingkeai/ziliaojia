90b895a87985a5f013db5cec8418b291cc11de16
binocean <1328463287@qq.com>
2020-09-23 22:14:25 +0800

    写了下详情页

--- miniprogram-1/pages/manager/detail/detail.js
+++ miniprogram-1/pages/manager/detail/detail.js
@@ -1,4 +1,5 @@
 // pages/manager/detail/detail.js
+import request from '../../../service/request.js'
 Page({
 
   /**
@@ -12,55 +13,27 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let id = 12
+    this.getMaterianInfo(id)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getMaterianInfo(id){
+    request({
+      url: 'Yunying/getMaterialInfo',
+      method: 'POST',
+      data: id
+    }).then(res =>{
+      let that = this
+      const { code, data } = res.data;
+      if (code === 200) {
+        this.setData({
+          materianInfo: data
+        })
+      } else {
+        console.log('获取失败');
+      }
+    }).catch(err=>{
+      console.log(err);          
+    })
   }
 })

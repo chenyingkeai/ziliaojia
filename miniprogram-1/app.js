bee454730c2180f5df2872f493030a8de0e5a395
chenyingkeai <1692313479@qq.com>
2020-09-26 19:23:24 +0800

    2020.9.26

--- miniprogram-1/app.js
+++ miniprogram-1/app.js
@@ -8,23 +8,49 @@ App({
 
     // 登录
     wx.login({
-      success: res => {
-        console.log(res.code);
-        
-        // console.log("denglule");
-        
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+      success: (res) => {
+        this.globalData.code = res.code
+        this.globalData.userInfo = res.userInfo
+        console.log("登陆");
+        console.log(this.globalData);
+      },
     })
-    // 获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
+          console.log("正在登陆");
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              let that =this
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
+              console.log(res);
+              console.log(this.globalData);
+              wx.request({
+                url: 'http://134.175.246.52:8080/wxLogin',
+                method:"POST",
+                data:{
+                  "code": this.globalData.code,
+                  "userImage": this.globalData.userInfo.avatarUrl,
+                  "userName": this.globalData.userInfo.nickName
+                },
+                header: {
+                  'content-type': 'application/json' // 默认值
+                },
+                success (res) {
+                  console.log("登陆成功")
+                  console.log("openid",res.data.data);
+                  that.globalData.openid=res.data.data.openId;
+                  that.globalData.userId=res.data.data.userId;
+                  try {
+                    wx.setStorageSync('openid', res.data.data.openId)
+                  } catch (e) {
+                    console.log("存入setStorageSync出错");
+                    console.log(e);
+                   }
+                }
+              })              
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -37,6 +63,9 @@ App({
     })
   },
   globalData: {
-    userInfo: null
-  }
+    userInfo: null,
+    code:null,
+    openid:null,
+    userId:null,
+  },
 })
--- miniprogram-1/pages/index/index.js
+++ miniprogram-1/pages/index/index.js
@@ -16,20 +16,7 @@ Page({
     selectList2:['语文','数学','英语','物理','化学','道德与法治'],
     selectList3:['知识点','教案','课件'],
     selectNum:"",//筛选
-    item:[
-      {
-        type:"试卷",
-        itemName:"广雅中学高中语文·必修三第二单元测试卷",
-        itemLook:200,
-        itemFollow:316,
-      },
-      {
-        type:"试卷",
-        itemName:"2222222广雅中学高中语文·必修三第二单元测试卷广雅中学高中语文·必修三第二单元测试卷",
-        itemLook:200,
-        itemFollow:316,
-      }
-    ]
+    zlList:[]
   },
   //事件处理函数
   // bindViewTap: function() {
@@ -65,8 +52,46 @@ Page({
           })
         }
       })
+    };
+    try {
+      var value = wx.getStorageSync('openid')
+      if (value) {
+        // Do something with return value
+        console.log("value",value);
+      }
+    } catch (e) {
+      // Do something when catch error
     }
+    this.getzlList();
   },
+  // 获取资料列表
+  getzlList(){
+    let that=this;
+    wx.request({
+      url: "http://134.175.246.52:8080/material/selectByGood",
+      method:"POST",
+      success(res) {
+        that.setData({
+          zlList:res.data.data
+        })
+      },
+    })
+  },
+  // 点赞资料
+  tapGood(){
+    let that=this;
+    wx.request({
+      url: "http://134.175.246.52:8080/material/setGood/{openId}/{zlId}?openId="+this.globalData.openId+"&zlId="+14,
+      method:"POST",
+      success(res) {
+        that.setData({
+          zlList:res.data.data
+        })
+      },
+    })
+
+  },
+  // 跳转到搜索页
   toSearch(){
     wx.navigateTo({
       url: '/pages/index/search/search',
@@ -75,6 +100,7 @@ Page({
       },
     })
   },
+  // 跳转到详情页
   toDetails(){
     wx.navigateTo({
       url: "/pages/index/details/details",
@@ -83,13 +109,43 @@ Page({
       },
     })
   },
+  // 获取个人信息
   getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    if (e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo
+      console.log(e.detail.userInfo)      
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true,
+        code:app.globalData.code
+      })
+      console.log(this.data.code);
+      let that =this
+      // 可以将 res 发送给后台解码出 unionId
+      wx.request({
+        url: 'http://134.175.246.52:8080/wxLogin',
+        method:"POST",
+        data:{
+          "code": this.globalData.code,
+          "userImage": this.globalData.userInfo.avatarUrl,
+          "userName": this.globalData.userInfo.nickName
+        },
+        success (res) {
+          console.log(res.data)
+          console.log("index登陆中");
+          app.globalData.openid=res.data.data.openId
+          app.globalData.userId=res.data.data.userId
+        }
+      })              
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      if (this.userInfoReadyCallback) {
+        this.userInfoReadyCallback(res)
+      }
+      wx.switchTab({
+        url: '../index1/index1'
+      })
+    }
   },
   onTap(e){
     let that = this
